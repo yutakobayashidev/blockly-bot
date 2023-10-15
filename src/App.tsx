e730ebb8dc17e9b5a90d2b862eb080a14921dd38
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-10-15 22:28:16 +0900

    category

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,13 @@
 
 import React from "react";
 import "./App.css";
-import BlocklyComponent, { Block, Value, Field, Shadow } from "./Blockly";
+import BlocklyComponent, {
+  Block,
+  Category,
+  Value,
+  Field,
+  Shadow,
+} from "./Blockly";
 
 import "./blocks/customblocks";
 import "./generator/generator";
@@ -47,30 +53,34 @@ function App() {
 </xml>
       `}
         >
-          <Block type="test_react_field" />
-          <Block type="test_react_date_field" />
-          <Block type="controls_ifelse" />
-          <Block type="logic_compare" />
-          <Block type="logic_operation" />
-          <Block type="controls_repeat_ext">
-            <Value name="TIMES">
-              <Shadow type="math_number">
-                <Field name="NUM">10</Field>
-              </Shadow>
-            </Value>
-          </Block>
-          <Block type="logic_operation" />
-          <Block type="logic_negate" />
-          <Block type="logic_boolean" />
-          <Block type="logic_null" disabled="true" />
-          <Block type="logic_ternary" />
-          <Block type="text_charAt">
-            <Value name="VALUE">
-              <Block type="variables_get">
-                <Field name="VAR">text</Field>
-              </Block>
-            </Value>
-          </Block>
+          <Category name="SenseBoxen" colour="#4F6272">
+            <Block type="test_react_field" />
+            <Block type="test_react_date_field" />
+            <Block type="controls_ifelse" />
+            <Block type="logic_compare" />
+            <Block type="logic_operation" />{" "}
+          </Category>
+          <Category name="SenseBoxen" colour="#4F6272">
+            <Block type="controls_repeat_ext">
+              <Value name="TIMES">
+                <Shadow type="math_number">
+                  <Field name="NUM">10</Field>
+                </Shadow>
+              </Value>
+            </Block>
+            <Block type="logic_operation" />
+            <Block type="logic_negate" />
+            <Block type="logic_boolean" />
+            <Block type="logic_null" disabled="true" />
+            <Block type="logic_ternary" />
+            <Block type="text_charAt">
+              <Value name="VALUE">
+                <Block type="variables_get">
+                  <Field name="VAR">text</Field>
+                </Block>
+              </Value>
+            </Block>
+          </Category>
         </BlocklyComponent>
       </header>
     </div>
